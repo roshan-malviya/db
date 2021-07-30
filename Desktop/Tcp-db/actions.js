8425f17e4fb9b8cdb25aedcee29940089cf50d3e
roshan-malviya <roshan19@navgurukul.org>
2021-07-30 11:34:49 +0530

    gitignore

---
+++ Desktop/Tcp-db/actions.js
@@ -0,0 +1,117 @@
+const fs = require('fs')
+const color = require('colors')
+
+
+
+const SET = (data) => {
+    switch (data.length) {
+        case 3:
+            store[data[1]] = data[2]
+            return ("OK\n".green)
+
+        default:
+            return (`set takes two inputs as arguments but get ${data.length - 1}\n`)
+    }
+}
+
+
+const GET = (data) => {
+
+
+    switch (data.length) {
+        case 2:
+            if (store[data[1]] == undefined) {
+                return ("key dosen't exist\n".yellow)
+            } else {
+                return (`${store[data[1]]}\n`.green)
+            }
+
+        default:
+            return (`get takes one inputs as arguments but get ${data.length - 1}\n`.green)
+
+    }
+
+
+}
+
+
+
+const EXPIRES = (data) => {
+
+    if (data.length == 3) {
+        if (!isNaN(data[2])) {
+            console.log(data[1])
+            if (store[data[1]] != undefined) {
+                setTimeout(function () {
+                    delete store[data[1]]
+                }, 1000 * data[2])
+                return ("OK\n".green)
+            } else {
+                return (`Key dosen't exist\n`.red)
+            }
+        } else {
+            return ('second argument should be a number\n'.green)
+        }
+    } else {
+        return (`expire takes 2 inputs given ${data.length - 1}\n`.red)
+    }
+}
+
+const SAVE = (data) => {
+
+    if (Object.keys(store).length === 0) {
+        return ('DB is empty'.blue)
+    } else {
+        const jsonData = JSON.stringify(store)
+        fs.writeFileSync('/tmp/data.json', jsonData)
+        console.log("saved")
+        return ("OK\n".green)
+    }
+}
+
+
+const LPUSH =(data)=>{
+
+    if ((store[data[1]]==undefined || store[data[1]]) &&  ! Array.isArray(store[data[1]])){
+        let arr = [data[2]]
+       store[data[1]]=arr;
+       
+    }else if (Array.isArray(store[data[1]])){
+        let arr1 = store[data[1]]
+        arr1.push(data[2])
+    }
+   return("OK\n".green)
+}
+
+
+const RPUSH =(data)=>{
+
+    if ((store[data[1]]==undefined || store[data[1]]) &&  ! Array.isArray(store[data[1]])){
+        let arr = [data[2]]
+       store[data[1]]=arr;
+       
+    }else if (Array.isArray(store[data[1]])){
+        let arr1 = store[data[1]]
+        arr1.unshift(data[2])
+    }
+   return("OK\n".green)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { SET, GET, EXPIRES, SAVE, LPUSH, RPUSH };
+
+
+
--- Desktop/Tcp-db/server.js
+++ Desktop/Tcp-db/server.js
@@ -1,48 +1,70 @@
-const { Socket } = require("dgram");
+const fs = require('fs')
 const colors = require('colors')
-const net = require("net")
+const net = require("net");
 const HOST = '127.0.0.1'
 const PORT = 8080;
+
+const SET= require('./actions').SET
+const GET= require('./actions').GET
+const EXPIRES= require('./actions').EXPIRES
+const SAVE= require('./actions').SAVE
+const LPUSH= require('./actions').LPUSH
+const RPUSH= require('./actions').RPUSH
+
+
+
+
 const server = net.createServer((Sock)=>{
-    let store1 ={}
+     fileExistence = fs.existsSync('/tmp/data.json')
+    if (fileExistence){
+        let kk = fs.readFileSync('/tmp/data.json','utf-8')
+        store = JSON.parse(kk)
+    }else{
+        store = {}
+    }
+
     console.log(`connected : ${Sock.remoteAddress} : ${Sock.remotePort}`)
     Sock.setEncoding('utf-8')
     Sock.on('data',(data)=>{
         
         console.log(`data ${Sock.remoteAddress} ${data}`)        
         let  data_ = data.trim().split(' ')
-        data_=data_.map((d)=>d.trim().toLowerCase())
-        if (data_[0]=='set'&& data_.length==3){
-            store1[data_[1]]=data_[2]
-            Sock.write('OK\n'.green)
-        }else if (data_.length==2){
-            if(data_[0]=='get'){
-            if (store1[data_[1]]!=undefined){
-                Sock.write(`${store1[data_[1]]}\n`)
-            }
-            else{
-                Sock.write("no data available\n".yellow)
-            }
-            }else if (data_[0]=='del'){
-                delete store1[data_[1]]
-                Sock.write('deleted\n'.yellow)
-            }
-            else{
-                Sock.write("Wrong command\n".yellow)
-            }
-        }else if (data_.length==1){
-            if(data_[0]=='flushall'){
-            store1={}
-            Sock.write("DELETED ALL DATA\n".yellow)
-            }else if (data_[0]=="quit"){
-                Sock.end("Good bye\n".rainbow)
-            }
+        data_=data_.map((d)=>d.trim())
+        console.log(store)
+        if(data_[0].toLowerCase()=='set'){
+            let aaa =SET(data_)
+            console.log(store)
+            Sock.write(aaa)
         }
-        else{
-            console.log("Error is here " )
-            Sock.write("Wrong input\n".red)
+        else if (data_[0].toLowerCase()=='get'){
+            let aaa1 = GET(data_)
+            console.log(typeof(aaa1))
+            Sock.write(aaa1)
+        }        else if (data_[0].toLowerCase()=='expires'){
+            let aaa1 = EXPIRES(data_)
+            Sock.write(aaa1)
+        }        else if (data_[0].toLowerCase()=='save'){
+            let aaa1 = SAVE(data_)
+            Sock.write(aaa1)
+        }else if(data_[0].toLowerCase()=='lpush'){
+            const res2 = LPUSH(data_)
+            Sock.write(res2)
+        }else if(data_[0].toLowerCase()=='rpush'){
+            const res2 = RPUSH(data_)
+            Sock.write(res2)
+        }else{
+            if (data_[0]==''){
+                Sock.write('\n')
+            }else(
+                Sock.write('wrong input\n'.red)
+            )
         }
     })
+
+
+
+
+
 }).listen(PORT,HOST)
 
 console.log(`server is listening on port ${PORT} `)
